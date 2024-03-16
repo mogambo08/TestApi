f30d4d285963093fc27f8a6f0097f572c88b6c23
mogambo08 <suatkaya08@hotmail.com>
2024-03-16 15:01:04 +0300

    Docker2

--- Api/Program.cs
+++ Api/Program.cs
@@ -10,13 +10,13 @@ builder.WebHost.UseUrls("http://*.1000");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
